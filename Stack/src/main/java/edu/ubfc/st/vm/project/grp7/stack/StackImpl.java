ea91570524efc32d43f5f1f9c62e502cb60d55a9
nicolasdeniset <nicolas2220@hotmail.fr>
2020-10-19 17:56:45 +0200

    Implementation de la stack

--- Stack/src/main/java/edu/ubfc/st/vm/project/grp7/stack/StackImpl.java
+++ Stack/src/main/java/edu/ubfc/st/vm/project/grp7/stack/StackImpl.java
@@ -9,23 +9,34 @@ public class StackImpl implements Stack {
         stack = new java.util.Stack<>();
     }
 
-    public void push(Quadruplet quadruplet){
-        stack.push(quadruplet);
+    public void push(Quadruplet quadruplet) throws InvalidQuadrupletException {
+        if(findQuadrupletInStack(quadruplet.getIdent()) == null && quadruplet.getIdent() != "") {
+            stack.push(quadruplet);
+        }
+        else {
+            throw new InvalidQuadrupletException(quadruplet,"Error, the quadruplet is empty.");
+        }
     }
 
     public Quadruplet pop(){
         return (Quadruplet) stack.pop();
     }
 
-    public void swap() {
-        Quadruplet quadruplet1 = (Quadruplet) stack.pop();
-        Quadruplet quadruplet2 = (Quadruplet) stack.pop();
+    public void swap() throws StackException {
+        if(stack.size() > 1) {
+            Quadruplet quadruplet1 = (Quadruplet) stack.pop();
+            Quadruplet quadruplet2 = (Quadruplet) stack.pop();
 
-        stack.push(quadruplet1);
-        stack.push(quadruplet2);
+            stack.push(quadruplet1);
+            stack.push(quadruplet2);
+        }
+        else {
+            throw new StackException(stack, "The stack doesn't contain enough entries to swap");
+        }
     }
 
-    public Quadruplet findQuadrupletInStack(String ident){
+    public Quadruplet findQuadrupletInStack(String ident)  {
+
         for( Object stackQuad : stack) {
             if(((Quadruplet) stackQuad).getIdent() == ident){
                 return (Quadruplet) stackQuad;
@@ -34,13 +45,16 @@ public class StackImpl implements Stack {
         return null;
     }
 
-    public void editQuadrupletInStack(String ident, Quadruplet quadruplet){
+    public void editQuadrupletInStack(String ident, Quadruplet quadruplet) throws StackException {
         Quadruplet quadrupletAChanger = findQuadrupletInStack(ident);
         if(quadrupletAChanger != null) {
-            quadruplet.setIdent(quadrupletAChanger.getIdent());
+            stack.findQuadrupletInStack(ident).setIdent(quadrupletAChanger.getIdent());
             quadruplet.setVal(quadrupletAChanger.getVal());
             quadruplet.setSorte(quadrupletAChanger.getSorte());
             quadruplet.setType(quadrupletAChanger.getType());
+        }
+        else {
+            throw new StackException(stack,"The quadruplet couldn't be found in the stack");
         }
     }
 
--- Stack/src/test/StackTest.java
+++
@@ -1,4 +0,0 @@
-package java.edu.ubfc.st.vm.project.grp7.stack;
-
-public class StackTest {
-}
