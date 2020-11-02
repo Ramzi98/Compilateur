53622c4d7341e961e6660aa4fe1f7d549a7db7b8
Karam Henni <karam.tenes@gmail.com>
2020-11-02 19:54:05 +0100

    Add JajaCode

---
+++ jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcWritelnNode.java
@@ -0,0 +1,15 @@
+package edu.ubfc.st.vm.project.grp7.jaja.code.ast.node;
+
+import edu.ubfc.st.vm.project.grp7.ast.ASTNode;
+
+public interface JcWritelnNode {
+    static JcWritelnNode.Builder builder() {
+        return new JcWritelnNode.Builder();
+    }
+
+    public class Builder extends ASTNode.NodeBuilder {
+        public JcWriteNode build() {
+            return new JcWritelnImpl(this.line, this.column);
+        }
+    }
+}
