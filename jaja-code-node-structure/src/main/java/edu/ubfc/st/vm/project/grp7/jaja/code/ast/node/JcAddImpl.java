9d3805344e74402fa1d6165b41e65f8e81c2bd12
Karam Henni <karam.tenes@gmail.com>
2020-11-06 18:46:48 +0100

    Fix Jajacode nodes

--- jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddImpl.java
+++ jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddImpl.java
@@ -4,7 +4,26 @@ import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeNode;
 import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeOperator;
 
 public class JcAddImpl extends JajaCodeOperator implements JcAddNode {
-    public JcAddImpl(int line, int column, JajaCodeNode leftOperand, JajaCodeNode rightOperand) {
+    private final JajaCodeNode next;
+
+    public JcAddImpl(int line, int column, JajaCodeNode leftOperand, JajaCodeNode rightOperand,JajaCodeNode next) {
         super(line, column,leftOperand, rightOperand);
+        this.next = next;
     }
+
+    @Override
+    public JajaCodeNode next() {
+        return this.next;
+    }
+
+    @Override
+    public JajaCodeNode children(int n) throws IndexOutOfBoundsException {
+        switch (n) {
+            case 0 : {return this.next; }
+            default: {throw new IndexOutOfBoundsException();}
+        }
+    }
+
+
+
 }
--- jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddNode.java
+++ jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddNode.java
@@ -1,21 +1,31 @@
 package edu.ubfc.st.vm.project.grp7.jaja.code.ast.node;
 
 import edu.ubfc.st.vm.project.grp7.ast.visitor.ASTVisitor;
+import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeNode;
 import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeOperatorNode;
 
 public interface JcAddNode extends JajaCodeOperatorNode {
+
+    JajaCodeNode next();
+
     @Override
     default void accept(ASTVisitor visitor) throws Exception {
         visitor.visit(this);
     }
 
-    static JcSupNode.Builder builder() {
-        return new JcSupNode.Builder();
+    static JcAddNode.Builder builder() {
+        return new JcAddNode.Builder();
     }
 
     class Builder extends JajaCodeOperatorNode.NodeBuilder<JcAddNode.Builder> {
+        JajaCodeNode next;
+        public JcAddNode.Builder next(JajaCodeNode next) {
+            this.next = next;
+            return this;
+        }
+
         public JcAddNode build() {
-            return new JcAddImpl(this.line, this.column,this.leftOperand, this.rightOperand);
+            return new JcAddImpl(this.line, this.column,this.leftOperand, this.rightOperand,this.next);
         }
     }
 }
