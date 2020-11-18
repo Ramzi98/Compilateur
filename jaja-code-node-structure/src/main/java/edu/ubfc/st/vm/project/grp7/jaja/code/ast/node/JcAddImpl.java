ee56baf1666cecf3db9d1f0f5937c793763e17ff
Karam Henni <karam.tenes@gmail.com>
2020-11-18 16:09:22 +0100

    Fix breakpoint

--- jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddImpl.java
+++ jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/node/JcAddImpl.java
@@ -1,15 +1,17 @@
 package edu.ubfc.st.vm.project.grp7.jaja.code.ast.node;
 
+import edu.ubfc.st.vm.project.grp7.ast.ASTNodeBreakpoint;
 import edu.ubfc.st.vm.project.grp7.ast.ASTNodeWithInfo;
+import edu.ubfc.st.vm.project.grp7.ast.Breakpoint;
 import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeNode;
 import edu.ubfc.st.vm.project.grp7.jaja.code.ast.JajaCodeOperator;
 
-public class JcAddImpl extends ASTNodeWithInfo implements JcAddNode {
+public class JcAddImpl extends ASTNodeBreakpoint implements JcAddNode {
     private final JajaCodeNode next;
 
 
-    public JcAddImpl(int line, int column,JajaCodeNode next) {
-        super(line, column);
+    public JcAddImpl(int line, int column, Breakpoint bp,JajaCodeNode next) {
+        super(line, column,bp);
         this.next = next;
     }
 
