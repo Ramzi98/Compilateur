f0e069072e55fb375cd0f7baefa0076ff2fc9245
Tchoulak Ramzi <29214961+Ramzi98@users.noreply.github.com>
2020-11-20 20:24:12 +0100

    Add Compiler package

--- jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/JajaCodeASTVisitor.java
+++ jaja-code-node-structure/src/main/java/edu/ubfc/st/vm/project/grp7/jaja/code/ast/JajaCodeASTVisitor.java
@@ -80,5 +80,34 @@ public abstract class JajaCodeASTVisitor implements ASTVisitor {
     }
 
     public abstract void visit(JcInitImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcAddImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcAincImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcAloadImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcAndImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcAstoreImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcCmpImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcDivImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcGotoImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcIfImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcIncImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcInvokeImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcLoadImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcMulImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcNegImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcNewarrayImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcNewImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcNopImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcNotImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcOrImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcPopImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcPushImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcReturnImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcStopImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcStoreImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcSubImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcSupImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcSwapImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcWriteImpl node) throws IllFormedNodeException, IOException;
+    public abstract void visit(JcWritelnImpl node) throws IllFormedNodeException, IOException;
 
 }
