5180e5e7c025383836f458c82a301eab1527360b
Karam Henni <karam.tenes@gmail.com>
2020-11-21 09:28:41 +0100

    Add compiler and compiler implementation

---
+++ Compiler/src/main/java/Compiler.java
@@ -0,0 +1,14 @@
+import edu.ubfc.st.vm.project.grp7.mini.jaja.ast.MiniJajaNode;
+
+public interface Compiler {
+
+
+
+    void compile() throws Exception;
+
+    public static Compiler CompilerBuilder(MiniJajaNode node){
+
+                return new CompilerImpl(node);
+
+    }
+}
---
+++ Compiler/src/main/java/CompilerImpl.java
@@ -0,0 +1,17 @@
+import edu.ubfc.st.vm.project.grp7.mini.jaja.ast.MiniJajaNode;
+
+public class CompilerImpl implements Compiler {
+
+    private final MiniJajaNode node;
+
+    CompilerImpl(MiniJajaNode node){
+        this.node = node;
+    }
+
+    @Override
+    public void compile() throws Exception {
+
+        CompilerVisitor visitor = new CompilerVisitor();
+        node.accept(visitor);
+    }
+}
