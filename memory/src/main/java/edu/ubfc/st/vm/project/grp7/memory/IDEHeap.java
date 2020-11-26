40aaf90322beda2d17219dd3faa6547dbac82c14
abonnea2 <antoine.bonneau@edu.univ-fcomte.fr>
2020-11-26 15:46:38 +0100

    naive heap implementation

--- memory/src/main/java/edu/ubfc/st/vm/project/grp7/memory/IDEHeap.java
+++ memory/src/main/java/edu/ubfc/st/vm/project/grp7/memory/IDEHeap.java
@@ -1,28 +1,82 @@
 package edu.ubfc.st.vm.project.grp7.memory;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+// TODO: 26/11/2020 Implémentation réelle du temps prenant en compte le cours : blocs en puissances de 2, Garbage collector, ...
 public class IDEHeap implements Heap {
+    private final Map<UUID, Object[]> objects;
+    private final Map<UUID, Integer> refs;
+    public IDEHeap() {
+        this.objects = new HashMap<>();
+        this.refs = new HashMap<>();
+    }
+
     @Override
-    public Object ajouterRef(Object ref, SORTE type) {
+    public Object ajouterRef(Object ref, SORTE type) throws IllegalArgumentException {
+        if (ref instanceof UUID && refs.containsKey(ref)) {
+            refs.put((UUID) ref, refs.get(ref) + 1);
+            return ref;
+        }
+            throw new IllegalArgumentException("Unknown ref");
+    }
+
+    @Override
+    public void affecterTas(Object ref, int index, Object val) throws IllegalArgumentException, IndexOutOfBoundsException {
+           if (objects.containsKey(ref)) {
+               objects.get(ref)[index] = val;
+           } else {
+               throw new IllegalArgumentException("Invalid ref on array");
+           }
+    }
+
+    @Override
+    public void retirerTas(Object ref, SORTE type) throws IllegalArgumentException {
+        if (ref instanceof UUID) {
+            if (refs.containsKey(ref)) {
+                int descriptors = refs.get(ref);
+                if (--descriptors == 0) {
+                    objects.remove(ref);
+                    refs.remove(ref);
+                }
+            } else {
+                throw new IllegalArgumentException("No UUID match with this ref");
+            }
+        } else {
+            throw new IllegalArgumentException("The ref doesn't exist in the heap");
+        }
+    }
+
+    @Override
+    public Object CreerTas(Object val, SORTE type) throws IllegalArgumentException {
+        if (val instanceof Integer && (Integer) val > 0) {
+            int size = (Integer) val;
+            UUID uuid = UUID.randomUUID();
+            refs.put(uuid, 1);
+            objects.put(uuid, new Object[size]);
+            return uuid;
+        } else {
+            throw new IllegalArgumentException("Array Size must be a positive integer");
+        }
+    }
+
+    @Override
+    public Object valeurTas(Object ref, int index) throws IllegalArgumentException, IndexOutOfBoundsException {
+        if(ref instanceof UUID) {
+            if (objects.containsKey(ref)) {
+                return objects.get(index)[index];
+            }
+        } else {
+            throw new IllegalArgumentException("Incorrect ref type");
+        }
         return null;
     }
 
     @Override
-    public void affecterTas(Object ref, int index, Object val) {
-
-    }
-
-    @Override
-    public void retirerTas(Object ref, SORTE type) {
-
-    }
-
-    @Override
-    public Object CreerTas(Object val, SORTE type) {
-        return null;
-    }
-
-    @Override
-    public Object valeurTas(Object val, int index) {
-        return null;
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{\n");
+        objects.forEach((k, v) -> builder.append(k.toString()).append(" : ").append(v).append("\n"));
+        return builder.append("}\n").toString();
     }
 }
