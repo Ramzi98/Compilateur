8b0c716c4fa6324160e5fef49a7dfebc069e4d30
vincent <vincent.tourneret.perso@gmail.com>
2020-11-26 12:04:18 +0100

    stack

--- memory/src/main/java/edu/ubfc/st/vm/project/grp7/memory/IDEStack.java
+++ memory/src/main/java/edu/ubfc/st/vm/project/grp7/memory/IDEStack.java
@@ -1,49 +1,106 @@
 package edu.ubfc.st.vm.project.grp7.memory;
 
+import java.util.ArrayList;
+
 public class IDEStack implements Stack {
+    private final SymbolDictionnary symbolDictionnary;
+    private final ArrayList<Quadruplet> quads ;
+    private int top;
+
+    public IDEStack(){
+        symbolDictionnary = new SymbolDictionnary();
+        quads = new ArrayList<>();
+        top = -1;
+    }
+
+    /**
+     * while (quad == null && top > 0){
+     *             quad = quads.get(top);
+     *             top--;
+     *         }
+     */
+
+
     @Override
     public Quadruplet peekFirst(String id) {
-        return null;
+        int index = symbolDictionnary.find(id);
+        if (index == -1){
+            return null;
+        }else{
+            return quads.get(index);
+        }
     }
 
     @Override
     public void empiler(Quadruplet q) {
-
+        quads.add(q);
+        if (! q.id().isEmpty()) {
+            symbolDictionnary.register(q.id(), top++);
+        }
     }
 
     @Override
-    public void depiler() {
+    public Quadruplet depiler() {
+        removeNulls();
+        if (top >= 0) {
+            Quadruplet quad = quads.get(top);
+            symbolDictionnary.unregister(quad.id());
+            quads.remove(top--);
+            removeNulls();
+            return quad;
+        }
+        return null;
+    }
 
+    private void removeNulls(){
+        Quadruplet quad = null;
+        while (quad == null && top >= 0){
+            quad = quads.get(top--);
+        }
     }
 
     @Override
     public void echanger() {
-
+        Quadruplet q1 = depiler();
+        Quadruplet q2 = depiler();
+        empiler(q1);
+        empiler(q2);
     }
 
     @Override
-    public void ajouterRef(Object ref, SORTE type) {
-
+    public void pushScope(String scope) {
+        symbolDictionnary.pushScope(scope);
     }
 
     @Override
-    public void pushStack(String scope) {
-
+    public void popScope() {
+        symbolDictionnary.popScope();
     }
 
-    @Override
-    public void popStack() {
-
-    }
-
+    /*
+    IdentVal(i,t, < i1, v1, o1,t1 > .m, s) = Si s == 0 alors < i, v1, var,t > .m
+    sinon < i1, v1, o1,t1 >.IdentVal(i,t, m, s  1)
+     */
     @Override
     public void identVal(String id, SORTE t, int s) {
+        removeNulls();
+        int index = symbolDictionnary.find(id);
+        if (index == top) {
 
+        } else {
+
+        }
     }
 
+
     @Override
-    public Quadruplet removeFirst(String id) {
-        return null;
+    public Quadruplet removeFirst(String id) throws IllegalArgumentException{
+        removeNulls();
+        int index = symbolDictionnary.find(id);
+        Quadruplet quadruplet = quads.get(index);
+        symbolDictionnary.unregister(quadruplet.id());
+        quads.remove(index);
+        return quadruplet;
     }
 
     @Override
---
+++ memory/src/test/java/SymbolDictionnary.java
@@ -0,0 +1,2 @@
+public class SymbolDictionnary {
+}
