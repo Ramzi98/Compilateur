d2910093b93726c92579b0e591f7955b0685b28c
vincent <vincent.tourneret.perso@gmail.com>
2020-11-27 10:50:06 +0100

    test stack continue

--- memory/src/test/java/edu/ubfc/st/vm/project/grp7/memory/IDEStackTest.java
+++ memory/src/test/java/edu/ubfc/st/vm/project/grp7/memory/IDEStackTest.java
@@ -4,65 +4,116 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class IDEStackTest {
-
     IDEStack ideStack;
-    IDEStack ideStack2;
+    SymbolDictionnary symbolDictionnary;
 
     @Before
-    public void init(){
-        ideStack = new IDEStack();
+    public void init() {
+        symbolDictionnary = mock(SymbolDictionnary.class);
+        ideStack = new IDEStack(symbolDictionnary);
     }
 
     @Test
-    public void peekFirstTest(){
-        ideStack.empiler(new Quadruplet("i",5,OBJ.VAR,SORTE.INT));
-        ideStack.empiler(new Quadruplet("j",6,OBJ.VAR,SORTE.INT));
-
-        Assert.assertEquals(ideStack.peekFirst("i").id(),"i");
-        Assert.assertEquals(ideStack.peekFirst("i").type(),SORTE.INT);
-        Assert.assertEquals(ideStack.peekFirst("i").val(),5);
-        Assert.assertEquals(ideStack.peekFirst("i").nature(),OBJ.VAR);
-
-        Assert.assertEquals(ideStack.peekFirst("j").id(),"j");
-        Assert.assertEquals(ideStack.peekFirst("j").type(),SORTE.INT);
-        Assert.assertEquals(ideStack.peekFirst("j").val(),6);
-        Assert.assertEquals(ideStack.peekFirst("j").nature(),OBJ.VAR);
+    public void depilerNull(){
+        assertThat(ideStack.depiler(),is(nullValue()));
     }
 
     @Test
-    public void echangerTest(){
-       /* ideStack.empiler(new Quadruplet("i",5,OBJ.VAR,SORTE.INT));
-        ideStack.empiler(new Quadruplet("j",6,OBJ.VAR,SORTE.INT));
+    public void depilerNullWithBadId(){
+        ideStack.empiler(new Quadruplet(null, true, null, null));
+        Quadruplet q = ideStack.depiler();
+        assertThat(q.nature(), is(nullValue()));
+        assertThat(q.id(), is(nullValue()));
+        assertThat(q.type(), is(nullValue()));
+        assertThat(q.val(), is(true));
+        assertThat(q.toString(),is("<w, true, w, w>"));
+    }
+
+    @Test
+    public void peekFirstTest() {
+        when(symbolDictionnary.find("i")).thenReturn(0);
+        when(symbolDictionnary.find("j")).thenReturn(1);
+
+        ideStack.empiler(new Quadruplet("i", 5, OBJ.VAR,SORTE.INT));
+        ideStack.empiler(new Quadruplet("j", 6, OBJ.VAR,SORTE.INT));
+
+        assertThat(ideStack.peekFirst("i").id(), is("i"));
+        assertThat(ideStack.peekFirst("i").type(), is(SORTE.INT));
+        assertThat(ideStack.peekFirst("i").val(), is(5));
+        assertThat(ideStack.peekFirst("i").nature(), is(OBJ.VAR));
+
+        assertThat(ideStack.peekFirst("j").id(), is("j"));
+        assertThat(ideStack.peekFirst("j").type(), is(SORTE.INT));
+        assertThat(ideStack.peekFirst("j").val(), is(6));
+        assertThat(ideStack.peekFirst("j").nature(), is(OBJ.VAR));
+    }
+
+    @Test
+    public void echangerTest() {
+        ideStack.empiler(new Quadruplet("i", 5, OBJ.VAR, SORTE.INT));
+        ideStack.empiler(new Quadruplet("j", 6, OBJ.VAR, SORTE.INT));
         ideStack.echanger();
-        ideStack.depiler();
 
-        Assert.assertEquals(ideStack.peekFirst("i").id(),"i");
-        Assert.assertEquals(ideStack.peekFirst("i").type(),SORTE.INT);
-        Assert.assertEquals(ideStack.peekFirst("i").val(),5);
-        Assert.assertEquals(ideStack.peekFirst("i").nature(),OBJ.VAR);
+        Quadruplet q = ideStack.depiler();
+        assertThat(q.id(), is("i"));
+        assertThat(q.val(), is(5));
+        assertThat(q.type(), is(SORTE.INT));
+        assertThat(q.nature(), is(OBJ.VAR));
 
-        Assert.assertEquals(ideStack.peekFirst("j"),null);*/
-
+        Quadruplet q2 = ideStack.depiler();
+        assertThat(q2.id(), is("j"));
+        assertThat(q2.val(), is(6));
+        assertThat(q2.type(), is(SORTE.INT));
+        assertThat(q2.nature(), is(OBJ.VAR));
     }
 
     @Test
-    public void toStringTest(){
-        ideStack.empiler(new Quadruplet("i",5,OBJ.VAR,SORTE.INT));
-        ideStack.empiler(new Quadruplet("j",6,OBJ.VAR,SORTE.INT));
+    public void toStringTest() {
+        ideStack.empiler(new Quadruplet("i", 5, OBJ.VAR, SORTE.INT));
+        ideStack.empiler(new Quadruplet("j", 6, OBJ.VAR, SORTE.INT));
 
         System.out.println(ideStack);
     }
 
-
     @Test
-    public void peekFirstNull(){
-        Assert.assertEquals(ideStack.peekFirst("i"),null);
+    public void peekFirstNull() {
+        //pas dans le dictionnaire de symbole
+        when(symbolDictionnary.find("i")).thenReturn(-1);
+        assertThat(ideStack.peekFirst("i"), is(nullValue()));
     }
 
+
+    @Test
+    public void removeFirstTest(){
+       /* ideStack.empiler(new Quadruplet("i", 5, OBJ.VAR, SORTE.INT));
+        ideStack.empiler(new Quadruplet("j", 6, OBJ.VAR, SORTE.INT));
+        ideStack.empiler(new Quadruplet("k", 7, OBJ.VAR, SORTE.INT));
+        ideStack.empiler(new Quadruplet("l", 8, OBJ.CST, SORTE.INT));
+
+        ideStack.removeFirst("k");
+        ideStack.removeFirst("j");
+
+        Quadruplet q = ideStack.depiler();
+        assertThat(q.id(), is("l"));
+        assertThat(q.val(), is(8));
+        assertThat(q.type(), is(SORTE.INT));
+        assertThat(q.nature(), is(OBJ.CST));
+
+        Quadruplet q2 = ideStack.depiler();
+        assertThat(q.id(), is("i"));
+        assertThat(q.val(), is(5));
+        assertThat(q.type(), is(SORTE.INT));
+        assertThat(q.nature(), is(OBJ.VAR));*/
+
+    }
 /**
  * private final SymbolDictionnary symbolDictionnary;
  *     private final ArrayList<Quadruplet> quads ;
