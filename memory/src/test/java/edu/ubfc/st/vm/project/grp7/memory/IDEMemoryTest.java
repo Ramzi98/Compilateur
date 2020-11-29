d9cb11e1c6b1c3f6c5a12407e1fc4c0d059d7752
abonnea2 <antoine.bonneau@edu.univ-fcomte.fr>
2020-11-29 10:29:09 +0100

    test base for mjj interpreter tests

--- memory/src/test/java/edu/ubfc/st/vm/project/grp7/memory/IDEMemoryTest.java
+++ memory/src/test/java/edu/ubfc/st/vm/project/grp7/memory/IDEMemoryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.*;
 
 
--- mini-jaja-interpreter/src/test/java/edu/ubfc/st/vm/project/grp7/mini/jaja/interpreter/MiniJajaInterpreterVisitorTest.java
+++ mini-jaja-interpreter/src/test/java/edu/ubfc/st/vm/project/grp7/mini/jaja/interpreter/MiniJajaInterpreterVisitorTest.java
@@ -1,6 +1,25 @@
 package edu.ubfc.st.vm.project.grp7.mini.jaja.interpreter;
 
+import edu.ubfc.st.vm.project.grp7.memory.IDEMemory;
+import edu.ubfc.st.vm.project.grp7.memory.Memory;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
 
 public class MiniJajaInterpreterVisitorTest {
+    private MiniJajaInterpreterVisitor mjjVisitor ;
+    private Memory memory;
+    private MJJInterpreterController controller;
+
+    @Before
+    public  void init(){
+        memory = mock(IDEMemory.class);
+        controller = mock(MJJInterpreterController.class);
+        mjjVisitor = new MiniJajaInterpreterVisitor(memory, controller);
+    }
+
 
 }
