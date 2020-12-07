16c19de7de0ca55979e6d59e29744c1f45418ded
abonnea2 <antoine.bonneau@edu.univ-fcomte.fr>
2020-12-07 15:16:52 +0100

    jjc interpreter debugger + invoke

---
+++ jaja-code-interpreter/src/test/java/edu/ubfc/st/vm/project/grp7/jaja/code/interpreter/JJCInterpreterVisitorTest.java
@@ -0,0 +1,11 @@
+package edu.ubfc.st.vm.project.grp7.jaja.code.interpreter;
+
+import org.junit.Test;
+import org.mockito.Spy;
+import org.junit.Before;
+import org.mockito.InOrder;
+import static org.mockito.Mockito.*;
+
+public class JJCInterpreterVisitorTest {
+
+}
