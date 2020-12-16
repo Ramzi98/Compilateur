c2c05c3bd556094c4dd4a6474b8e3f2946843958
vincent <vincent.tourneret.perso@gmail.com>
2020-12-16 14:31:35 +0100

    commit some stuff to fix gui

--- graphic-interface/src/main/java/edu/ubfc/st/vm/project/grp7/graphic/DebugOffWaiter.java
+++ graphic-interface/src/main/java/edu/ubfc/st/vm/project/grp7/graphic/DebugOffWaiter.java
@@ -4,19 +4,11 @@ import edu.ubfc.st.vm.project.grp7.debug.DebugListener;
 
 public class DebugOffWaiter implements DebugListener.Waiter {
     @Override
-    public void waitForUser(boolean debug) throws InterruptedException {
-
-    }
+    public void waitForUser(boolean debug) throws InterruptedException {}
 
     @Override
-    public void nextBreakpoint() {
-
-    }
+    public void nextBreakpoint() {}
 
     @Override
-    public void nextStep() {
-
-    }
-
-
+    public void nextStep() {}
 }
