6073168bb9a4ac244a9ebb13d36702d22d941a89
Tchoulak Ramzi <29214961+Ramzi98@users.noreply.github.com>
2020-12-16 18:36:15 +0100

    Add Integration Tests

--- graphic-interface/src/test/java/ProjetExampleParserTypeCheckerTest.java
+++ graphic-interface/src/test/java/ProjetExampleParserTypeCheckerTest.java
@@ -7,7 +7,7 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
     protected TypeChecker typeChecker;
 
     @Test
-    public void TestClassfactTypeCheckerParser() throws Exception {
+    public void Test_Class_fact_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","fact");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
@@ -19,7 +19,7 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
 
 
     @Test
-    public void TestClassQuicksortTypeCheckerParser() throws Exception {
+    public void Test_Class_Quicksort_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","quicksort");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
@@ -33,7 +33,7 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
 
 
     @Test
-    public void TestClass1TypeCheckerParser() throws Exception {
+    public void Test_Class_1_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","1");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
@@ -44,7 +44,7 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
     }
 
     @Test
-    public void TestClassSynonymieTypeCheckerParser() throws Exception {
+    public void Test_Class_Synonymie_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","synonymie");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
@@ -55,7 +55,7 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
     }
 
     @Test
-    public void TestClassTasTypeCheckerParser() throws Exception {
+    public void Test_Class_Tas_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","tas");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
@@ -67,8 +67,19 @@ public class ProjetExampleParserTypeCheckerTest extends BaseTest {
 
 
     @Test
-    public void TestClasstestexempleTypeCheckerParser() throws Exception {
+    public void Test_Class_test1_TypeChecker_Parser() throws Exception {
         TestConstructor testConstructor = new TestConstructor("ProjectExample","test1");
+        parser = testConstructor.getParser();
+        walker.walk(listener, parser.classe());
+
+        ClasseNode classeNode = (ClasseNode) listener.getRoot();
+        typeChecker = new TypeCheckerImpl(classeNode);
+        typeChecker.typeCheck();
+    }
+
+    @Test
+    public void Test_Class_test2_TypeChecker_Parser() throws Exception {
+        TestConstructor testConstructor = new TestConstructor("ProjectExample","test2");
         parser = testConstructor.getParser();
         walker.walk(listener, parser.classe());
 
